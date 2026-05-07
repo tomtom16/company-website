7efb54d9f851326428bb32dbb9e3242524e676d7
thomas.rotter <thomas.rotter@cloud-catalyst.io>
2026-05-07 12:17:33 +0200

    removed api key

--- image.sh
+++ image.sh
@@ -12,7 +12,7 @@ VERSION=latest
 
 #az acr login --name "${REGISTRY}"
 #docker buildx build --platform linux/arm64 --push --build-arg "VERSION=${VERSION}" -t "${REGISTRY}/${IMAGE_NAME}:latest" .
-docker buildx build --platform linux/arm64 --load --build-arg "VERSION=${VERSION}" -t "${REGISTRY}/${IMAGE_NAME}:latest" .
+docker buildx build --platform linux/arm64 --load --build-arg "VERSION=${VERSION}" --build-arg PUBLIC_GOOGLE_MAPS_API_KEY=AIzaSyCTpaGmDUTlHdwgfT0keKjChSzBZKx44HQ --build-arg PUBLIC_GOOGLE_PLACE_ID=ChIJaX6xB90FbUcRU58MeUXU3KQ -t "${REGISTRY}/${IMAGE_NAME}:latest" .
 #docker push "${REGISTRY}/${IMAGE_NAME}" --all-tags
 
 cd - || exit 1
